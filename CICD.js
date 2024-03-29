e19a7f7d23fb9aa47e99af7d2d8bca200a04cd39
zhangkr <zhangkr>
2024-03-29 09:44:07 +0800

    upd

--- CICD.js
+++ CICD.js
@@ -11,9 +11,9 @@ const config = {
   // 本地需要上传的文件
   uploadFile: './dist',
   // 服务器路径
-  remotePath: `/home/polarday/h5-retail/dist`,
+  remotePath: `xxx`,
   // 服务器备份路径
-  backupFolder: `/home/polarday/h5-retail/backup`,
+  backupFolder: `xxx/backup`,
   // 作者
   author: 'CICD'
 };
